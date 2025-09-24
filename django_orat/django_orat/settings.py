2cc6221fdbc47ac22ae09d8dbf494d0ba0dea8ba
LAB13_USER ON WS28 <L13X16W28@LabHCI.net>
2025-09-24 17:30:14 +0800

    feature(app_name): setup templates

--- django_orat/django_orat/settings.py
+++ django_orat/django_orat/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_orat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates' / 'app_orat'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
